993d3f03a5c8d5ee971b4e1fe1e73c87b50c0bed
bebeto22 <tec.josec@gmail.com>
2020-06-13 14:09:57 -0300

    added eslintrc.js config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		browser  : true,
+		commonjs : true,
+		es2020   : true,
+	},
+	extends: [
+		`airbnb`,
+		`eslint:recommended`,
+		`plugin:react/recommended`,
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 11,
+	},
+	plugins: [
+		`react`,
+	],
+	rules: {
+		"react/jsx-filename-extension" : [1, { extensions: [`.js`, `.jsx`] }],
+		'consistent-return'            : `error`,
+		'brace-style'                  : [`error`, `allman`],
+		'space-in-parens'              : [`error`, `always`],
+		'linebreak-style'              : [`off`],
+		indent                         : [`error`, `tab`],
+		'no-tabs'                      : [`error`, { allowIndentationTabs: true }],
+		quotes                         : [`error`, `backtick`],
+		'key-spacing'                  : [`error`, {
+			align: {
+				beforeColon : true,
+				afterColon  : true,
+				on          : `colon`,
+			},
+		}],
+	},
+};
