ddd71c30b0e9405d109a73f01bb139dcb8f81529
bebeto22 <tec.josec@gmail.com>
2020-09-19 17:48:16 -0300

    add item component created

---
+++ src/components/AddLogItem.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+	Card, Form, Row, Col, Button,
+} from 'react-bootstrap';
+
+const AddLogItem = ( { addLogItem } ) =>
+{
+	const [text, setText] = useState( `` );
+	const [user, setUser] = useState( `` );
+	const [priority, setPriority] = useState( `` );
+
+	const onSubmit = ( e ) =>
+	{
+		e.preventDefault();
+
+		addLogItem( { text, user, priority } );
+	};
+
+	return (
+
+		<Card className="mt-5 mb-3">
+			<Card.Body>
+				<Form onSubmit={onSubmit}>
+
+					<Row className="my-3">
+
+						<Col>
+
+							<Form.Control placeholder="Log" value={text} onChange={( e ) => setText( e.target.value )} />
+
+						</Col>
+
+					</Row>
+
+					<Row>
+
+						<Col>
+
+							<Form.Control placeholder="User" value={user} onChange={( e ) => setUser( e.target.value )} />
+
+						</Col>
+
+						<Col>
+
+							<Form.Control as="select" value={priority} onChange={( e ) => setPriority( e.target.value )}>
+
+								<option value="0">Select Priority</option>
+								<option value="low">Low</option>
+								<option value="moderate">Moderate</option>
+								<option value="high">High</option>
+
+							</Form.Control>
+
+						</Col>
+
+					</Row>
+
+					<Row className="my-3">
+
+						<Col>
+
+							<Button type="submit" variant="secondary" block>Add Log</Button>
+
+						</Col>
+
+					</Row>
+
+				</Form>
+			</Card.Body>
+		</Card>
+
+	);
+};
+
+AddLogItem.propTypes = {
+	addLogItem: PropTypes.func.isRequired,
+};
+
+export default AddLogItem;
