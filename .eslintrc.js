4c681a46834764339ecb52e5aed7d680a6b9389a
bebeto22 <tec.josec@gmail.com>
2020-09-19 16:42:04 -0300

    eslint updated & prop-types added

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
 		`react`,
 	],
 	rules: {
+        "no-underscore-dangle"         : [ `off` ],
+        "react/forbid-prop-types"      : [ `off` ],
         "react/jsx-filename-extension" : [1, { extensions: [`.js`, `.jsx`] }],
         "react/jsx-indent"             : [ 'off' ],
 		'consistent-return'            : `error`,
