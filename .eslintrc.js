8148ba38be59bbca84be8c9388b38562bcf4c814
bebeto22 <tec.josec@gmail.com>
2020-09-26 12:41:47 -0300

    eslint ignore rule added

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
 		`react`,
 	],
 	rules: {
+    "react/jsx-indent-props"           : [ `off` ],
         "no-underscore-dangle"         : [ `off` ],
         "react/forbid-prop-types"      : [ `off` ],
         "react/jsx-filename-extension" : [1, { extensions: [`.js`, `.jsx`] }],
